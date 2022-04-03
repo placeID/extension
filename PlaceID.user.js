f72770254ee7231b288c1f7dbb4543c84380e7b5
xilla! <102897181+xillaLove@users.noreply.github.com>
2022-04-04 02:15:34 +0700

    update

--- PlaceID.user.js
+++ PlaceID.user.js
@@ -12,7 +12,7 @@ if (window.top !== window.self) {
     window.addEventListener('load', () => {
         // Load the image
         const image = document.createElement("img");
-        image.src = "https://raw.githubusercontent.com/placeID/extension/main/overlay2.png";
+        image.src = "https://i.imgur.com/4PHkngC.png";
         image.onload = () => {
             image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
         };
